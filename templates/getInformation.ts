10d3d80f5d83419d7fd1c9e2bb041857b0251b2c
tadanobutaaaaa <youyah1004@gmail.com>
2024-12-02 15:03:42 +0900

    タイトル取得からurl取得に変更

--- templates/getInformation.ts
+++ templates/getInformation.ts
@@ -2,11 +2,10 @@ export function getInformation() {
     function mainProcessing() {
         const elements = document.getElementsByClassName("mb-16 bg-white p-16 desktop:rounded-8 desktop:py-24 desktop:px-40")
         for(let i = 0; i < elements.length; i++) {
-            const imageDiv: string = elements[i].querySelector("img:first-child").getAttribute('src')
-            console.log(imageDiv)
-
-            const titleName = elements[i].getElementsByClassName("text-text-default font-bold typography-16 !preserve-half-leading mb-8 break-all")[0].textContent
-            console.log(titleName)
+            const imageSrc = elements[i].querySelector("img:first-child").getAttribute('src')
+            const imagehref = elements[i].querySelector("a:first-child").getAttribute("href")
+            console.log(imageSrc)
+            console.log(imagehref)
 
             const fileNameGroup = elements[i].getElementsByClassName("mt-16 desktop:flex desktop:justify-between desktop:items-center")
             for(let j = 0; j < fileNameGroup.length; j++) {
